2a15e314b3fb0cda82f44842989abd098667b65c
Bryan Arendt <barendt@covermymeds.com>
2018-05-19 07:50:22 -0400

    super brittle test

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule CoverMyElixir.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:httpoison, "~> 1.0"}
+      {:httpoison, "~> 1.0"},
+      {:mix_test_watch, "~> 0.6", only: :dev, runtime: false}
     ]
   end
 
