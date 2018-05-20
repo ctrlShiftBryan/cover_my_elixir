18cff7a88558ed9d2fce2f52a244fe49b9951a43
Bryan Arendt <barendt@covermymeds.com>
2018-05-20 07:15:45 -0400

    make all tests pass

--- test/cover_my_elixir/accounts/accounts_test.exs
+++ test/cover_my_elixir/accounts/accounts_test.exs
@@ -3,6 +3,11 @@ defmodule CoverMyElixir.AccountsTest do
 
   alias CoverMyElixir.Accounts
 
+  import Mock
+  alias CoverMyElixir.Stubs.Accounts.UserApi.Users
+  alias HTTPoison.Response
+  @mock_response {:ok, %Response{status_code: 200, body: Users.all() |> Poison.encode!()}}
+
   describe "users" do
     alias CoverMyElixir.Accounts.User
 
@@ -22,20 +27,22 @@ defmodule CoverMyElixir.AccountsTest do
     test "list_users/0 returns all users" do
       user_fixture()
 
-      users = [
-        %{"name" => "Leanne Graham", "phone" => "1-770-736-8031 x56442"},
-        %{"name" => "Ervin Howell", "phone" => "010-692-6593 x09125"},
-        %{"name" => "Clementine Bauch", "phone" => "1-463-123-4447"},
-        %{"name" => "Patricia Lebsack", "phone" => "493-170-9623 x156"},
-        %{"name" => "Chelsey Dietrich", "phone" => "(254)954-1289"},
-        %{"name" => "Mrs. Dennis Schulist", "phone" => "1-477-935-8478 x6430"},
-        %{"name" => "Kurtis Weissnat", "phone" => "210.067.6132"},
-        %{"name" => "Nicholas Runolfsdottir V", "phone" => "586.493.6943 x140"},
-        %{"name" => "Glenna Reichert", "phone" => "(775)976-6794 x41206"},
-        %{"name" => "Clementina DuBuque", "phone" => "024-648-3804"}
-      ]
+      with_mock HTTPoison, get: fn _url -> @mock_response end do
+        users = [
+          %{"name" => "Leanne Graham", "phone" => "1-770-736-8031 x56442"},
+          %{"name" => "Ervin Howell", "phone" => "010-692-6593 x09125"},
+          %{"name" => "Clementine Bauch", "phone" => "1-463-123-4447"},
+          %{"name" => "Patricia Lebsack", "phone" => "493-170-9623 x156"},
+          %{"name" => "Chelsey Dietrich", "phone" => "(254)954-1289"},
+          %{"name" => "Mrs. Dennis Schulist", "phone" => "1-477-935-8478 x6430"},
+          %{"name" => "Kurtis Weissnat", "phone" => "210.067.6132"},
+          %{"name" => "Nicholas Runolfsdottir V", "phone" => "586.493.6943 x140"},
+          %{"name" => "Glenna Reichert", "phone" => "(775)976-6794 x41206"},
+          %{"name" => "Clementina DuBuque", "phone" => "024-648-3804"}
+        ]
 
-      assert Accounts.list_users() == {:ok, users}
+        assert Accounts.list_users() == {:ok, users}
+      end
     end
 
     test "get_user!/1 returns the user with given id" do
--- test/cover_my_elixir_web/controllers/page_controller_test.exs
+++ test/cover_my_elixir_web/controllers/page_controller_test.exs
@@ -1,8 +1,15 @@
 defmodule CoverMyElixirWeb.PageControllerTest do
   use CoverMyElixirWeb.ConnCase
 
+  import Mock
+  alias CoverMyElixir.Stubs.Accounts.UserApi.Users
+  alias HTTPoison.Response
+  @mock_response {:ok, %Response{status_code: 200, body: Users.all() |> Poison.encode!()}}
+
   test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    with_mock HTTPoison, get: fn _url -> @mock_response end do
+      conn = get(conn, "/")
+      assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    end
   end
 end
