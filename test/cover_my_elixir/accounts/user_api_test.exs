ae886a3974f805ef2f0934b13b7a3fdb687ad655
Bryan Arendt <barendt@covermymeds.com>
2018-05-20 06:56:35 -0400

    alias UserApi

--- test/cover_my_elixir/accounts/user_api_test.exs
+++ test/cover_my_elixir/accounts/user_api_test.exs
@@ -1,9 +1,11 @@
 defmodule CoverMyElixir.Tests.Accounts.UserApi do
   use ExUnit.Case
 
+  alias CoverMyElixir.Accounts.UserApi
+
   test "can call the api and get back something with " do
     # make sure we get :ok
-    assert {:ok, users} = CoverMyElixir.Accounts.UserApi.all()
+    assert {:ok, users} = UserApi.all()
 
     # check the keys of each item to make sure the are what we expect
     keys = fn list ->
